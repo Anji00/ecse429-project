056f41b88785d0c866c7fd210d6f316d8d79d077
faizachy <faizachy18@gmail.com>
2024-10-06 16:08:52 -0400

    gradle project setup

---
+++ src/test/java/TodosTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TodosTest {
+
+
+}
